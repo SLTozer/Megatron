63a895c918f24023d851ff8d3ef3b141a82a253e
Melamoto <Melamoto@gmail.com>
2015-10-31 17:24:20 +0000

    Adjusted calculate weights to return best particle

--- calculateWeights.m
+++ calculateWeights.m
@@ -1,11 +1,13 @@
-function [ weights, avgWeight ] = calculateWeights( particles, distances, sensorError )
-% This function returns a vector of normalized real weights and the average
-% un-normalized weight, and accepts botSim[], a vector of distances, and a
-% real standard deviation for the sensor error.
+function [ weights, avgWeight, bestIndex ] = calculateWeights( particles, distances, sensorError )
+% This function returns a vector of normalized real weights, the average un-normalized weight,
+% and the index of the particle with highest weight. It accepts botSim[], a vector of distances,
+% and a real standard deviation for the sensor error.
 
 minDistance = min(distances);
 count = length(particles);
 totalWeight = 0;
+
+bestWeight = 0;
 
 weights(count,1) = 0;
 % Calculate weights for each particle
@@ -15,6 +17,10 @@ for i = 1:count
     [partDists, ~] = particles(i).ultraScan();
     partMinDist = min(partDists);
     weights(i) = normalpdf(partMinDist, minDistance, sensorError);
+    if weights(i) > bestWeight
+        bestWeight = weights(i);
+        bestIndex = i;
+    end
     totalWeight = totalWeight + weights(i);
 end
 % Set average (un-normalized) weight
